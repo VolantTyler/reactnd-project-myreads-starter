bf0d5fc6ce140b51a646d4c31093a4b29cb574f8
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-09-12 13:26:57 -0400

    feat: search results display accurate book images

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -58,7 +58,7 @@ class SearchBook extends Component {
                     <li key={book.id}>
                         <div className="book">
                             <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+                                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
                                 <div className="book-shelf-changer">
                                 <select>
                                     <option value="move" disabled>Move to...</option>
@@ -70,7 +70,7 @@ class SearchBook extends Component {
                                 </div>
                             </div>
                             <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.author}</div>
+                            <div className="book-authors">{book.authors}</div>
                         </div>
                     </li>
                 ))}
