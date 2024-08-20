a6c37ae2b379ca82e9dde615757713365da0f883
Tyler Stahl <tylerstahl@gmail.com>
2024-08-20 10:33:16 -0400

    test coverage for search function

--- tests/move-shelf.spec.js
+++ tests/move-shelf.spec.js
@@ -1,27 +1,53 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('has page title and shelf titles', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test.describe("move shelf", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+  });
 
-  await expect(page).toHaveTitle(/MyReads/);
-  await expect(page.getByRole('heading', { name: 'Currently Reading'})).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Want to Read'})).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Previously Read'})).toBeVisible();
-});
+  test("has page title and shelf titles", async ({ page }) => {
+    await expect(page).toHaveTitle(/MyReads/);
+    await expect(
+      page.getByRole("heading", { name: "Currently Reading" })
+    ).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Want to Read" })
+    ).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Previously Read" })
+    ).toBeVisible();
+  });
 
+  test("move book between shelves", async ({ page }) => {
+    const book1 = page.getByTestId("book-list-item").nth(1);
+    await expect(book1).toHaveClass("book");
 
-test('move book between shelves', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+    const bookLinux = page
+      .getByRole("listitem")
+      .filter({ hasText: "The Linux Command Line" });
+    await expect(bookLinux).toContainText("currentlyReading");
+    await bookLinux.getByRole("combobox").selectOption("wantToRead");
 
-  const book1 = page.getByTestId('book-list-item').nth(1)
-  await expect(book1).toHaveClass('book')
+    await expect(bookLinux).toContainText("wantToRead");
+    await expect(bookLinux.getByText("wantToRead")).not.toBeVisible();
+  });
 
-  const bookLinux = page.getByRole('listitem').filter({ hasText: 'The Linux Command Line'})
-  await expect(bookLinux).toContainText('currentlyReading')
-  await bookLinux.getByRole('combobox').selectOption('wantToRead');
+  test("search for a book", async ({ page }) => {
+    await page.getByRole("link", { name: "Add a book" }).click();
+    await expect(page.url()).toContain("/search");
 
-  await expect(bookLinux).toContainText('wantToRead')
-  await expect(bookLinux.getByText('wantToRead')).not.toBeVisible()
+    await page.getByPlaceholder("Search by title or author").fill("literary");
 
+    const bookLiterary = page
+      .getByRole("listitem")
+      .filter({ hasText: "Story in Literary Fiction" });
+    await bookLiterary.getByRole("combobox").selectOption("wantToRead");
+
+    // update project to return to shelf without back button, or give confirmation that it worked
+    await page.getByRole("link", { name: "Close" }).click();
+
+    await expect(page.url()).not.toContain("/search");
+    await expect(bookLiterary).toContainText("wantToRead");
+  });
 });
